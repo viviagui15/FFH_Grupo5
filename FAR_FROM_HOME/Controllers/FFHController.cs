413432669c3405e50e7be68e2c18db8f4d778d6b
viviana <viviana.aguilera15@gmail.com>
2020-10-31 14:54:42 -0300

    Version 1

---
+++ FAR_FROM_HOME/Controllers/FFHController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FAR_FROM_HOME.EF; 
+
+namespace FAR_FROM_HOME.Controllers
+{
+    public class FFHController : Controller
+    {
+        DATABASE_FFHEntities contexto;
+        // GET: Usuario
+        public FFHController()
+        {
+            contexto = new DATABASE_FFHEntities();
+        }
+        // GET: FFH
+        public ActionResult Index()
+        {
+            List<USUARIODT> usuarios = contexto.USUARIODT.ToList();
+            return View(usuarios);
+        }
+
+        public ActionResult Grilla()
+        {
+            return View();
+        }
+    }
+}
---
+++ FAR_FROM_HOME/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FAR_FROM_HOME.MvcApplication" Language="C#" %>
